4a2400d0f654e5358a6a9080545b05c5735325ee
jjxh <21540138@qq.com>
2014-10-23 01:12:08 +0800

    a

--- src/AnimationLayer.js
+++ src/AnimationLayer.js
@@ -41,6 +41,8 @@ var AnimationLayer = cc.Layer.extend({
 	init:function () {
 		this._super();
 		
+		var winsize = cc.director.getWinSize();
+		
 		//初始化物理世界
 		this.initPhysics();
 
@@ -48,22 +50,19 @@ var AnimationLayer = cc.Layer.extend({
 		this.spriteOwner = new cc.PhysicsSprite(res.Worker_png);
 		//---test physics start---
 		var contentSize = this.spriteOwner.getContentSize();
-		this.body = new cp.Body(1, cp.momentForBox(1, contentSize.width/5, contentSize.height/5));
-		this.space.addBody(this.body);
+		this.body = new cp.StaticBody();
+		this.body.setPos(cc.p(winsize.width - 226, winsize.height / 2 + 27));
 		this.shape = new cp.BoxShape(this.body, contentSize.width/5, contentSize.height/5);
 		this.space.addShape(this.shape);
 		this.spriteOwner.setBody(this.body);
-//		this.scheduleUpdate();
+		this.scheduleUpdate();
 		//---test physics end---
-		
 		
 		this.spriteEnemy = cc.Sprite.create(res.Worker2_png);
 		
-		
-		var winsize = cc.director.getWinSize();
 		this.spriteOwner.attr({x: winsize.width - 226, y: winsize.height});
 		this.spriteEnemy.attr({x: winsize.width /2 - 250, y: winsize.height});
-//		this.body.setPos( cp.v(winsize.width, winsize.height));
+		this.body.setPos(cc.p(300, 300));
 		
 		//this.initAction();
 		this.laughingAction = this.initAction(res.laught_plist, res.laught_png, res.spriteSheet, 50, 68);
@@ -143,21 +142,36 @@ var AnimationLayer = cc.Layer.extend({
 		shape.setCollisionType(1);
 
 		//木块
+//		var width = 20.0;
+//		var height = 20.0;
+//		var mass = 0.5*FLUID_DENSITY*width*height;
+//		var moment = cp.momentForBox(mass, width, height);
+//
+//		body = space.addBody( new cp.Body(mass, moment));
+//		body.setPos( cp.v(580, 250));
+//		this.waterBoxBody = body;
+//
+//		shape = space.addShape( new cp.BoxShape(body, width, height));
+//		shape.setFriction(0.8);
+		//----------------水面结束---------------
+	},
+	
+	throwBanana: function(){
 		var width = 20.0;
 		var height = 20.0;
 		var mass = 0.5*FLUID_DENSITY*width*height;
 		var moment = cp.momentForBox(mass, width, height);
 
-		body = space.addBody( new cp.Body(mass, moment));
-		body.setPos( cp.v(580, 250));
-		body.setVel( cp.v(0, -100));
-		body.setAngVel( 1 );
-		this.waterBoxBody = body;
+		var body = this.space.addBody( new cp.Body(mass, moment));
+		body.setPos( cc.p(580, 250));
 
-		shape = space.addShape( new cp.BoxShape(body, width, height));
+		var shape = this.space.addShape( new cp.BoxShape(body, width, height));
 		shape.setFriction(0.8);
-		space.addCollisionHandler( 1, 0, null, this.waterPreSolve, null, null);
-		//----------------水面结束---------------
+		
+		var speed = 20;
+		var x = this.boxDirectionX * speed * Math.cos(110*Math.PI/180);
+		var y = speed * Math.sin(60*Math.PI/180);
+		body.applyImpulse(cp.v(x,y), cp.v(0, 0));
 	},
 	
 	initDebugMode: function () {
@@ -173,6 +187,7 @@ var AnimationLayer = cc.Layer.extend({
 	},
 	onTouchEnded:function (touch, event) {
 		event.getCurrentTarget().laught();
+		event.getCurrentTarget().throwBanana();
 	},
 	laught:function (){
 		cc.log("laughing");
@@ -188,71 +203,3 @@ var AnimationLayer = cc.Layer.extend({
 		this.space.step(1/60.0);
 	}
 });
-
-AnimationLayer.prototype.waterPreSolve = function(arb, space) {
-	var shapes = arb.getShapes();
-	var water = shapes[0];
-	var poly = shapes[1];
-
-	var body = poly.getBody();
-
-	// Get the top of the water sensor bounding box to use as the water level.
-	var level = water.getBB().t;
-
-	// Clip the polygon against the water level
-	var count = poly.getNumVerts();
-
-	var clipped = [];
-
-	var j=count-1;
-	for(var i=0; i<count; i++) {
-		var a = body.local2World( poly.getVert(j));
-		var b = body.local2World( poly.getVert(i));
-
-		if(a.y < level){
-			clipped.push( a.x );
-			clipped.push( a.y );
-		}
-
-		var a_level = a.y - level;
-		var b_level = b.y - level;
-
-		if(a_level*b_level < 0.0){
-			var t = Math.abs(a_level)/(Math.abs(a_level) + Math.abs(b_level));
-
-			var v = cp.v.lerp(a, b, t);
-			clipped.push(v.x);
-			clipped.push(v.y);
-		}
-		j=i;
-	}
-
-	// Calculate buoyancy from the clipped polygon area
-	var clippedArea = cp.areaForPoly(clipped);
-
-	var displacedMass = clippedArea*FLUID_DENSITY;
-	var centroid = cp.centroidForPoly(clipped);
-	var r = cp.v.sub(centroid, body.getPos());
-
-	var dt = space.getCurrentTimeStep();
-	var g = space.gravity;
-
-	// Apply the buoyancy force as an impulse.
-	body.applyImpulse( cp.v.mult(g, -displacedMass*dt), r);
-
-	// Apply linear damping for the fluid drag.
-	var v_centroid = cp.v.add(body.getVel(), cp.v.mult(cp.v.perp(r), body.w));
-	var k = 1; //k_scalar_body(body, r, cp.v.normalize_safe(v_centroid));
-	var damping = clippedArea*FLUID_DRAG*FLUID_DENSITY;
-	var v_coef = Math.exp(-damping*dt*k); // linear drag
-//	var v_coef = 1.0/(1.0 + damping*dt*cp.v.len(v_centroid)*k); // quadratic drag
-	body.applyImpulse( cp.v.mult(cp.v.sub(cp.v.mult(v_centroid, v_coef), v_centroid), 1.0/k), r);
-
-	// Apply angular damping for the fluid drag.
-	var w_damping = cp.momentForPoly(FLUID_DRAG*FLUID_DENSITY*clippedArea, clipped, cp.v.neg(body.p));
-	body.w *= Math.exp(-w_damping*dt* (1/body.i));
-
-//	cc.log("waterPreSolve"+body.getMass());
-	body.setMass(body.getMass()+0.00015);
-	return true;
-};
